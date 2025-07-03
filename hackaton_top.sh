0ec0b4591511024356c31eb198c8f8aa03066e1b
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-07-02 17:08:00 -0700

    Create hackaton_top.sh

---
+++ hackaton_top.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# ============================================================================
+# shell script to backup hackaton_top.sv, then wget latest test code
+# Source: STEAM Clown - www.steamclown.org 
+# GitHub: https://github.com/jimTheSTEAMClown/verilog
+# Hacker: Jim Burnham - STEAM Clown, Engineer, Maker, Propmaster & Adrenologist 
+# This example code is licensed under the CC BY-NC-SA 4.0, GNU GPL and EUPL
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+# https://eupl.eu/
+# Program/Design Name:		hackaton_top.sh
+#
+# Copy This wget
+# wget -O hackathon_top.sv https://raw.githubusercontent.com/verilog-meetup/verilog-hackathon-education-kit-manual/refs/heads/main/SV-Examples/pb2led_test.sv
+# ============================================================================
+
+
+  echo "----------------------------------------------------"
+  if [ -e "/home/$USER/gowin/basic-graphics-music/hackaton/problems/testSV/hackaton_top.sv" ]; then
+    echo "File exists."
+    BACKUP_FILE="hackathon_top_$(date +%Y%m%d_%H%M%S).sv"
+    mv /home/$USER/gowin/basic-graphics-music/hackaton/problems/testSV/hackathon_top.sv /home/$USER/gowin/basic-graphics-music/hackaton/problems/testSV/$BACKUP_FILE
+  else
+    echo "File does not exist."
+ fi
+ 
+wget -O hackathon_top.sv https://raw.githubusercontent.com/verilog-meetup/verilog-hackathon-education-kit-manual/refs/heads/main/SV-Examples/pb2led_test.sv
+
+echo '  ____  _____  _  _  ____ '
+echo ' (  _ \(  _  )( \( )( ___) '
+echo '  )(_) ))(_)(  )  (  )__) '
+echo ' (____/(_____)(_)\_)(____) '
+echo "----------------------------------------------------"
+echo " "
+echo "----------------------------------------------------"
+echo '  _  _  ____  _  _  ____    ___  ____  ____  ____  ___ '
+echo ' ( \( )( ___)( \/ )(_  _)  / __)(_  _)( ___)(  _ \/ __) '
+echo '  )  (  )__)  )  (   )(    \__ \  )(   )__)  )___/\__ \ '
+echo ' (_)\_)(____)(_/\_) (__)   (___/ (__) (____)(__)  (___/ '
+echo "----------------------------------------------------"
+echo " "
+echo "----------------------------------------------------"
+                                                      
